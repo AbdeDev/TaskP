fd0ad34ac96dd4840288ae2f437c11d67ff3d0cf
AbdeDev <98222128+AbdeDev@users.noreply.github.com>
2024-09-17 06:36:52 +0200

    💄improve ui & add new features✨

--- task-manager/app/components/LoginButton.tsx
+++ task-manager/app/components/LoginButton.tsx
@@ -7,9 +7,12 @@ export default function LoginButton() {
 
   if (session) {
     return (
-      <div>
-        <p>Bienvenue, {session.user?.name}!</p>
-        <button onClick={() => signOut()} className="p-2 bg-red-500 text-white">
+      <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-4">
+        <p className="text-gray-300">Bienvenue, {session.user?.name}!</p>
+        <button
+          onClick={() => signOut()}
+          className="px-4 py-2 bg-gradient-to-r from-red-500 to-red-600 text-white font-semibold rounded-md shadow-md hover:from-red-600 hover:to-red-700 transition-all duration-300"
+        >
           Déconnexion
         </button>
       </div>
@@ -17,7 +20,10 @@ export default function LoginButton() {
   }
 
   return (
-    <button onClick={() => signIn('github')} className="p-2 bg-black text-white">
+    <button
+      onClick={() => signIn('github')}
+      className="px-4 py-2 bg-gradient-to-r from-black to-gray-800 text-white font-semibold rounded-md shadow-md hover:from-gray-800 hover:to-gray-900 transition-all duration-300"
+    >
       Connexion avec GitHub
     </button>
   );
